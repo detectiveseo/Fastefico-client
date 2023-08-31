49a169fd2450dae29ba26569173c9c3d6967cc1d
detectiveseo <detectiveseo1@gmai.com>
2023-08-31 12:08:51 +0600

    popular Item added

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Hero from './HomeComponents.jsx/Hero';
 import AboutusSection from './HomeComponents.jsx/AboutusSection';
 import Chefs from './HomeComponents.jsx/Chefs';
+import PopularItem from './HomeComponents.jsx/PopularItem';
 
 const Home = () => {
     return (<>
         <Hero></Hero>
         <AboutusSection></AboutusSection>
         <Chefs></Chefs>
+        <PopularItem />
     </>
     );
 };
